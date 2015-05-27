b13a3b3ea457ced72e956b80cb8d0b6ecf49c284
Jake <dynogic@gmail.com>
2015-05-27 11:37:12 -0700

    Introducing Telegram Settings

---
+++ Disa.Framework.Telegram.Mobile/Localize.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Resources;
+using System.Globalization;
+using System.Reflection;
+
+namespace Disa.Framework.Telegram.Mobile
+{
+    public class Localize
+    {
+        private static ResourceManager _cachedResourceManager;
+        private static CultureInfo _cachedCultureInfo;
+
+        private static string Locale()
+        {
+            return Platform.GetCurrentLocale();
+        }
+
+        public static string GetString(string key) 
+        {
+            if (_cachedCultureInfo == null || _cachedResourceManager == null)
+            {
+                _cachedCultureInfo = new CultureInfo(Locale());
+                _cachedResourceManager = new ResourceManager("Disa.Framework.Telegram.Mobile.Resx.AppResources", typeof(Localize).GetTypeInfo().Assembly);
+            }
+            return _cachedResourceManager.GetString(key, _cachedCultureInfo);
+        }
+    }
+}
+
---
+++ Disa.Framework.Telegram.Mobile/Settings.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Disa.Framework.Mobile;
+using Xamarin.Forms;
+
+namespace Disa.Framework.Telegram.Mobile
+{
+    [PluginSettingsUI(typeof(Telegram))]
+    public class Settings : IPluginPage
+    {
+        public Page Fetch(Service service)
+        {
+            NavigationPage navigationPage;
+//            if (ServiceManager.IsManualSettingsNeeded(service))
+//            {
+//                navigationPage = new NavigationPage(Setup.Fetch(service));
+//            }
+//            else 
+            {
+                navigationPage = new NavigationPage(new Main(service));
+            }
+            return navigationPage;
+        }
+
+        private class Main : ContentPage
+        {
+            private Label _status;
+            private Label _info;
+            private Button _activate;
+            private Button _setupWizard;
+            private Button _profileSettings;
+            private StackLayout _toolsLayout;
+
+            public Main(Service service) 
+            {
+                var running = ServiceManager.IsRunning(service);
+                var internetConnected = Platform.HasInternetConnection();
+
+                _status = new Label();
+                if (running)
+                {
+                    _status.Text = Localize.GetString("TelegramRunning");
+                    _status.TextColor = Color.Green;
+                }
+                else
+                {
+                    _status.Text = Localize.GetString("TelegramNotRunning");
+                    _status.TextColor = Color.Red;
+                }
+                _status.Font = Font.SystemFontOfSize(20);
+                _status.XAlign = TextAlignment.Center;
+
+                _info = new Label();
+                if (running)
+                {
+                    _info.Text = Localize.GetString("TelegramAllGood");
+                }
+                else
+                {
+                    if (internetConnected)
+                    {
+                        _info.Text = Localize.GetString("TelegramPleaseStart");
+                    }
+                    else
+                    {
+                        _info.Text = Localize.GetString("TelegramPleaseStartNoInternet");
+                    }
+                }
+                _info.Font = Font.SystemFontOfSize(16);
+                _info.XAlign = TextAlignment.Center;
+
+                _toolsLayout = new StackLayout();
+                _toolsLayout.Orientation = StackOrientation.Vertical;
+                _toolsLayout.VerticalOptions = LayoutOptions.EndAndExpand;
+                _toolsLayout.Spacing = 10;
+                _setupWizard = new Button();
+                _setupWizard.Text = Localize.GetString("TelegramSetupWizard");
+                _setupWizard.TextColor = Color.White;
+                _setupWizard.BackgroundColor = Color.FromHex("c50923");
+                _setupWizard.Clicked += async (sender, e) =>
+                    {
+                        //await Navigation.PushAsync(Setup.Fetch(service));
+                    };
+                if (running)
+                {
+                    _profileSettings = new Button();
+                    _profileSettings.Text = Localize.GetString("TelegramProfileSettings");
+                    _profileSettings.TextColor = Color.White;
+                    _profileSettings.BackgroundColor = Color.FromHex("77D065");
+                    _profileSettings.Clicked += async (sender, e) =>
+                        {
+                            //await Navigation.PushAsync(new ProfileSettings(service));
+                        };
+                    _toolsLayout.Children.Add(_profileSettings);
+                }
+                _toolsLayout.Children.Add(_setupWizard);
+
+                var stackLayout = new StackLayout();
+                stackLayout.Spacing = 10;
+                stackLayout.Padding = 25;
+                stackLayout.VerticalOptions = LayoutOptions.FillAndExpand;
+                var children = stackLayout.Children;
+                children.Add(_status);
+                children.Add(_info);
+                children.Add(_toolsLayout);
+
+
+                Content = new ScrollView { Content = stackLayout };
+                Title = Localize.GetString("TelegramSettingsTitle");
+            }
+        }
+    }
+}
+
