f9239c46cc22ffbf3486f7bd6d8b5c0c0ec46279
Jake Dynogic <dynogic@gmail.com>
2015-06-05 17:22:41 -0700

    Started implementing state system for updates

--- Disa.Framework.Telegram/SharpMTProto/Messaging/Handlers/UpdatesHandler.cs
+++ Disa.Framework.Telegram/SharpMTProto/Messaging/Handlers/UpdatesHandler.cs
@@ -13,6 +13,14 @@ namespace SharpMTProto.Messaging.Handlers
 {
     public class UpdatesHandler : ResponseHandler<IUpdates>
     {
+        public class State
+        {
+            public uint Date { get; set; }
+            public uint Pts { get; set; }
+            public uint Qts { get; set; }
+            public uint Seq { get; set; }
+        }
+
         private readonly TLRig _tlRig;
 
         public UpdatesHandler(TLRig tlRig)
@@ -21,7 +29,10 @@ namespace SharpMTProto.Messaging.Handlers
         }
 
         public EventHandler<List<object>> OnUpdate;
+
         public EventHandler OnUpdateTooLong;
+
+        public EventHandler<State> OnUpdateState;
 
         private void RaiseOnUpdateTooLong()
         {
@@ -36,6 +47,14 @@ namespace SharpMTProto.Messaging.Handlers
             if (OnUpdate != null)
             {
                 OnUpdate(this, updates);
+            }
+        }
+
+        private void RaiseOnUpdateState(State state)
+        {
+            if (OnUpdateState != null)
+            {
+                OnUpdateState(this, state);
             }
         }
 
@@ -56,27 +75,71 @@ namespace SharpMTProto.Messaging.Handlers
             }
 
             var updateList = new List<object>();
+            var state = new State();
 
             if (updateShort != null)
             {
+                state.Date = updateShort.Date;
                 updateList.Add(updateShort.Update);
             }
             else if (updateShortMessage != null)
             {
+                state.Date = updateShortMessage.Date;
+                state.Pts = updateShortMessage.Pts;
+                state.Seq = updateShortMessage.Seq;
                 updateList.Add(updateShortMessage);
             }
             else if (updateShortChatMessage != null)
             {
+                state.Date = updateShortChatMessage.Date;
+                state.Pts = updateShortChatMessage.Pts;
+                state.Seq = updateShortChatMessage.Seq;
                 updateList.Add(updateShortChatMessage);
             }
             else if (updatesCombined != null)
             {
+                state.Date = updatesCombined.Date;
+                state.Seq = updatesCombined.Seq;
                 updateList.AddRange(updatesCombined.Updates);
             }
             else if (updates != null)
             {
+                state.Date = updates.Date;
+                state.Seq = updates.Seq;
                 updateList.AddRange(updates.UpdatesProperty);
             }
+
+            foreach (var update in updateList)
+            {
+                var newMessage = update as UpdateNewMessage;
+                var readMessages = update as UpdateReadMessages;
+                var deleteMessages = update as UpdateDeleteMessages;
+                var restoreMessages = update as UpdateRestoreMessages;
+                var encryptedMessages = update as UpdateNewEncryptedMessage;
+
+                if (newMessage != null)
+                {
+                    state.Pts = newMessage.Pts;
+                }
+                else if (readMessages != null)
+                {
+                    state.Pts = readMessages.Pts;
+                }
+                else if (deleteMessages != null)
+                {
+                    state.Pts = deleteMessages.Pts;
+                }
+                else if (restoreMessages != null)
+                {
+                    state.Pts = restoreMessages.Pts;
+                }
+                else if (encryptedMessages != null)
+                {
+                    state.Qts = encryptedMessages.Qts;
+                }
+            }
+
+            RaiseOnUpdateState(state);
 
             //TODO: GZIP
 
