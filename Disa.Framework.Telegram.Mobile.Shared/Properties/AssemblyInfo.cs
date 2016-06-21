1aaf6aef3338f0e2a64c76fa5fe147b5afe2fd5c
Jake Louw <jake@disa.im>
2016-06-20 17:44:45 -0700

    Fixed AssemblyInfo

--- Disa.Framework.Telegram.Mobile.Shared/Properties/AssemblyInfo.cs
+++ Disa.Framework.Telegram.Mobile.Shared/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("House")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Disa.Framework.Telegram.Shared/Properties/AssemblyInfo.cs
+++ Disa.Framework.Telegram.Shared/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("jake")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
