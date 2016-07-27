31dbd870ed6956e3f495e12e706359247f8e12ed
Jake Louw <jake@disa.im>
2016-07-27 16:19:32 -0700

    Added more OSS

---
+++ Disa.Framework.Mobile/PhoneEntryRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Disa.Framework.Mobile;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(PhoneEntry), typeof(PhoneEntryRenderer))]
+namespace Disa.Framework.Mobile
+{
+    public class PhoneEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                var editText = Control;
+
+                editText.InputType = Android.Text.InputTypes.ClassPhone;
+            }
+        }
+    }
+}
+
