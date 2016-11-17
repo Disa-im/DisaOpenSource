ffd3921cd808a270dbfa9568db7ac9f1e3c9eb07
Jake Louw <jake@disa.im>
2016-11-17 12:54:34 -0500

    Implemented IAnnouncement interface

---
+++ Disa.Framework.Shared/IAnnouncement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Disa.Framework
+{
+	public interface IAnnouncement
+	{
+		string GetAnnouncementMessage();
+
+		bool IsAnnouncementReoccuring();
+
+		int GetAnnouncementRecurringInterval();
+
+		string GetAnnouncementName();
+	}
+}
