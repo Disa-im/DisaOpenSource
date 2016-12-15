37a57c23efad32d194791c69a6245441928771b4
bill <bill@disa.im>
2016-12-15 16:22:40 -0500

    Channel interim commit - starting INewChannel/NewChannel.

---
+++ Disa.Framework.Shared/INewChannel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Disa.Framework
+{
+    [DisaFramework]
+    public interface INewChannel
+    {
+        Task FetchBubbleGroup(Contact.ID[] contactIds, Action<BubbleGroup> result);
+
+        Task FetchBubbleGroupAddress(Tuple<Contact, Contact.ID>[] contacts, Action<bool, string> result);
+    }
+}
+
