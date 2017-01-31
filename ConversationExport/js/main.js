eeaa070c08ab5bdac00114d42769ed0529f8ac45
Naved Merchant <naved.merchant@gmail.com>
2017-01-31 10:55:28 -0800

    Updated Conversation Export, to support cache information

--- ConversationExport/js/main.js
+++ ConversationExport/js/main.js
@@ -11,9 +11,8 @@ $(window).load(function() {
     });
 });
 
-
 //Exported bubbles go here
-angular.module("app", []).controller('BubbleController', ['$scope', 'exportedBubbles', function ($scope, exportedBubbles) {
+angular.module("app", []).controller('BubbleController', ['$scope', 'exportedBubbles', 'exportedBubblesCache', function ($scope, exportedBubbles, exportedBubblesCache) {
     $scope.bubbles = exportedBubbles.bubbles.reverse();
 
     $scope.formatDate = function (timestamp) {
@@ -29,10 +28,42 @@ angular.module("app", []).controller('BubbleController', ['$scope', 'exportedBub
         return formattedTime;
     };
 
-    $scope.getPath = function(path) {
+    $scope.isGroup = function(){
+        if(exportedBubblesCache.cache.Participants.length > 0)
+            return true;
+        else
+            return false;
+    }
+
+    $scope.getParticipantName = function(address){
+        var participants = exportedBubblesCache.cache.Participants;
+        for(var participant in participants){
+            if(participants[participant].Address === address)
+            {
+                return participants[participant].Name;
+            }
+        }
+        return "Unknown";
+    }
+
+    $scope.getPath = function(path,type) {
         var filename = path.replace(/^.*[\\\/]/, '');
+        if(type === "image")
+            return "media/images/" + filename;
+        if(type === "audio")
+            return "media/audio/" + filename;
+        if(type === "video")
+            return "media/videos/" + filename;
+        if(type === "file")
+            return "media/files/" + filename;
         return "media/" + filename;
     };
+}]);
+
+angular.module("app").controller('NavbarController', ['$scope', 'exportedBubblesCache', function ($scope, exportedBubblesCache) {
+    $scope.getConversationTitle = function(){
+        return exportedBubblesCache.cache.Name;
+    }
 }]);
 
 angular.module("app").filter("trustUrl", ['$sce', function ($sce) {
