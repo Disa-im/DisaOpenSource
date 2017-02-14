22cf574de1371d797ea357544ef85a44c40d1330
bill <bill@disa.im>
2017-02-14 06:40:29 -0500

    Initial implementation for mentions.

---
+++ Disa.Framework.Shared/IMentions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Disa.Framework
+{
+    [DisaFramework]
+    public interface IMentions
+    {
+        //
+        // Original interface - for these you can use the following test methodology:
+        //
+        // // Does the service implement the required interface?
+        // var newChannelUi = service as INewChannel
+        // if (newChannelUi != null)
+        // {
+        //     .
+        //     .
+        //     .
+        // }
+        //
+
+        /// <summary>
+        /// Set the result of the <see cref="Action"/> as the token string to recognize
+        /// for popping up a selection dialog for a mention.
+        /// </summary>
+        /// <param name="result"><see cref="Action"/> on which the result should be set.</param>
+        /// <returns>A new <see cref="Task"/> that sets the result <see cref="Action"/>.</returns>
+        Task GetMentionsToken(Action<string> result);
+
+        //
+        // Begin interface extensions below. For these you must use the following test methodology
+        // or something similar:
+        // 
+        // // Do we have the required method?
+        // if(DisaFrameworkMethods.Missing(service, DisaFrameWorkMethods.INewChannelXxx)
+        // {
+        //     return;
+        // }
+        //
+        // // Ok to proceed
+        //
+
+    }
+}
+
---
+++ Disa.Framework.Telegram.Shared/UI/Mentions.cs
@@ -0,0 +1,21 @@
+﻿using SharpMTProto;
+using SharpTelegram.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Disa.Framework.Telegram
+{
+    public partial class Telegram : IMentions
+    {
+        public Task GetMentionsToken(Action<string> result)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                result("@");
+            });
+        }
+    }
+}
+
