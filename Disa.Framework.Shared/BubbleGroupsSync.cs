d4794a32e5456e9cceaa1ce88f063c3e187b444d
Jake <jake@disa.im>
2017-10-19 14:33:26 -0400

    Implementation 3

--- Disa.Framework.Shared/BubbleGroupsSync.cs
+++ Disa.Framework.Shared/BubbleGroupsSync.cs
@@ -7,7 +7,7 @@ namespace Disa.Framework
 {
     public class BubbleGroupsSync
     {
-        // TODO: move out to own class when Category system is well defined
+        //TODO: move out to own class when Category system is well defined
         public class Category
         {
             
@@ -30,11 +30,17 @@ namespace Disa.Framework
 
         public class Cursor
         {
+            private enum Result { NoCardinalityChange, NoChange, Change }
+
+            public bool IsBusy { get; private set; }
+
             private readonly List<BubbleGroup> _list;
             private readonly int _pageSize;
-            private readonly IEnumerator<bool> _enumerator;
+            private readonly IEnumerator<Result> _enumerator;
 
             private bool _refreshState;
+            private Action _postAction;
+            private Action _action;
 
             public Cursor(List<BubbleGroup> list, int pageSize)
             {
@@ -43,54 +49,107 @@ namespace Disa.Framework
                 _enumerator = LoadBubblesInternal().GetEnumerator();
             }
 
-            public void Refresh()
+            public void Schedule(Action action)
+            {
+                _action = action;
+            }
+
+            public bool Refresh()
             {
                 lock (_enumerator)
                 {
-                    _refreshState = true;
-                    _enumerator.MoveNext();
-                    _refreshState = false;
+                    var tempList = _list.ToList();
+                    try
+                    {
+                        _refreshState = true;
+                        _enumerator.MoveNext();
+                        return _enumerator.Current == Result.Change;
+                    }
+                    catch (Exception ex)
+                    {
+                        Utils.DebugPrint("BubbleGroupsSync", "Failed to refresh: " + ex);
+                        _list.Clear();
+                        _list.AddRange(tempList);
+                        return true;
+                    }
+                    finally
+                    {
+                        _refreshState = false;
+                        try
+                        {
+                            if (_action != null)
+                            {
+                                _action();    
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Utils.DebugPrint("BubbleGroupsSync", "Failed to execute action: " + ex);
+                        }
+                        finally
+                        {
+                            _action = null;
+                        }
+                    }
                 }
             }
 
-            public Task LoadBubbles()
+            public Task<bool> LoadBubbles(Action postAction = null)
             {
-                return Task.Factory.StartNew(() =>
+                return Task<bool>.Factory.StartNew(() =>
                 {
                     lock (_enumerator)
                     {
-                        _enumerator.MoveNext();
+                        var tempList = _list.ToList();
+                        try
+                        {
+                            IsBusy = true;
+                            _postAction = postAction;
+                            _enumerator.MoveNext();
+                            return _enumerator.Current != Result.NoCardinalityChange;
+                        }
+                        catch (Exception ex)
+                        {
+                            Utils.DebugPrint("BubbleGroupsSync", "Failed to load bubbles " + ex);
+                            _list.Clear();
+                            _list.AddRange(tempList);
+                            return false;
+                        }
+                        finally
+                        {
+                            IsBusy = false;
+                            _postAction = null;
+                        }
                     }
                 });
             }
 
-            private IEnumerable<bool> LoadBubblesInternal()
+            private IEnumerable<Result> LoadBubblesInternal()
             {
+                var doneQueryingAgents = false;
                 var count = _pageSize;
-                //var iteration = 0;
                 var servicesFinished = new Dictionary<Service, bool>();
                 while (true)
                 {
+                    var originalCardinality = _list.Count;
                     var bubbleGroups = BubbleGroupManager.DisplayImmutable;
                     //FIXME: ConvoList in UI will call this method before BubbleGroupManager is ready.
                     //       Fix this race condition.
                     if (!bubbleGroups.Any())
                     {
-                        yield return true;
+                        yield return Result.NoChange;
                         continue;
                     }
-                    //if (bubbleGroups.Count != 0 && count > bubbleGroups.Count)
-                    //{
-                    //    yield break;
-                    //}
                     SortListByTime(bubbleGroups);
                     if (!_refreshState)
                     {
                         count += _pageSize;
-                        //iteration++;
                     }
                     var page = bubbleGroups.Take(count).ToList();
-                    if (!_refreshState)
+                    var lazy = _list.Where(x => x.Lazy);
+                    page.AddRange(lazy);
+                    SortListByTime(page);
+                    if (!_refreshState && !doneQueryingAgents)
                     {
                         var bubbleGroupsToQuery = new List<BubbleGroup>();
                         for (int i = page.Count - 1; i >= 0; i--)
@@ -103,8 +162,7 @@ namespace Disa.Framework
                             }
                             if (group != null)
                             {
-                                var existing = bubbleGroupsToQuery.FirstOrDefault(x => x.Service == group.Service) !=
-                                               null;
+                                var existing = bubbleGroupsToQuery.FirstOrDefault(x => x.Service == group.Service) != null;
                                 if (!existing)
                                 {
                                     bubbleGroupsToQuery.Add(group);
@@ -147,25 +205,43 @@ namespace Disa.Framework
                             }
                             SortListByTime(page);
                             page = page.Take(count).ToList();
-                            _list.Clear();
-                            _list.AddRange(page);
                             if (servicesFinished.Count == bubbleGroupsRunningAgentToQuery.Count)
                             {
-                                yield break;
+                                //TODO: doneQueryingAgents needs to be set to false when a service is started
+                                doneQueryingAgents = true;
                             }
                         }
-                        else
+                    }
+                    _list.Clear();
+                    _list.AddRange(page);
+                    try
+                    {
+                        if (_postAction != null)
                         {
-                            _list.Clear();
-                            _list.AddRange(page);
+                            _postAction();
                         }
+                    }
+                    catch (Exception ex)
+                    {
+                        Utils.DebugPrint("BubbleGroupsSync",
+                                            "Failed to run post action: " + ex);
+                    }
+                    if (_refreshState)
+                    {
+                        //TODO: if the list hasn't changed return false to optimize UI updates
+                        yield return Result.Change;   
                     }
                     else
                     {
-                        _list.Clear();
-                        _list.AddRange(page);
+                        if (originalCardinality == _list.Count)
+                        {
+                            yield return Result.NoCardinalityChange;
+                        }
+                        else
+                        {
+                            yield return Result.Change;
+                        }
                     }
-                    yield return true;
                 }
             }   
         }
